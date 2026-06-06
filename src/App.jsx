6b751560f4683c236c47ff10f9af02b4609e3e3b
Luc1oN <shanedenis.connolly@gmail.com>
2026-06-06 19:18:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ToastProvider } from './components/ui/toast'
 import AppLayout from './pages/AppLayout'
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
-        <BrowserRouter basename="/SlowPour">
+        <HashRouter>
           <Routes>
             <Route element={<AppLayout />}>
               <Route path="/" element={<Welcome />} />
@@ -35,7 +35,7 @@ export default function App() {
               <Route path="/admin" element={<Admin />} />
             </Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ToastProvider>
     </QueryClientProvider>
   )
